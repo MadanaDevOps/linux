b6afa2159540bec829931f584746472e4e80fb9f
madana <madana@gmail.com>
2025-04-16 21:20:01 +0530

    jenkins configuration

---
+++ jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yum update -y
+yum install wget vim tar make unzip -y
+yum install java-17-openjdk-devel fontconfig git -y
+cd /opt
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install jenkins
+sudo systemctl daemon-reload
+firewall-cmd --permanent --add-port=808/tcp
+firewall-cmd --reload
+sudo systemctl start Jenkins
+sudo systemctl enable jenkins
+systemctl status Jenkins --no-pager
+
