3046e466d0fbb71dfea1345092c833d35446e27c
madana <madana@gmail.com>
2025-04-23 02:44:21 +0530

    text

---
+++ file.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+free -h
+cpuinfo
+rm -rf file1
+tykjggf
+hhmmmm
+trewqdgjlljl
+trewqasfgj
+yukjkh
+ojojlk
+rtfjhgklj;
+jjhg
+kk
+kkk
+tyyhg
+rrr
+ooo
+rrrtt
+rwwrr
+uuio
+trewq
+oiuu
+
+
