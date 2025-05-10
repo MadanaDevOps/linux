fdf3818cec21d70f957f36a2c8b0bd05ecf8356a
madana <madana@gmail.com>
2025-05-10 07:22:50 +0530

    test

---
+++ file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+free -h
+cpuinfo
+rm -rf file1
+tykjggf
+hhmmmm
+trewqdgjlljl
+trewqasfgj
+yukjkh
+ojojlk
+rtfjhgklj;
+jjhg
+kk
+kkk
+tyyhg
+rrr
+ooo
+rrrtt
+rwwrr
+uuio
+trewq
+oiuu
+rwkmds
+reifef,l
+                
---
+++ madan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cat << EOF > filename.txt
+This is line one.
+This is line two.
+EOF
+
