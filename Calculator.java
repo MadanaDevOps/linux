b8373df9145950cffb1aebe3cff63775ce67b4b1
madana <madana@gmail.com>
2025-05-31 14:32:06 +0530

    project 1

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0)
+            throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b;
+    }
+
+    public int power(int base, int exponent) {
+        return (int) Math.pow(base, exponent);
+    }
+}
+
